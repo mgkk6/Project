78c19a4a20c0ba70833662f2b0fbd375a974e89b
mgkk6 <velpeter98@gmail.com>
2021-08-20 19:28:52 +0300

    записал 2 функции, которые с помощью функции get_random_letter_and_number_element(number) будут создавать строку типа "рандомная строка.рандомная строка", только сейчас заметил, что функции и массивы немного не так назвал, но лень исправлять. Функция get_array_of_random_elements_negative(lengthOfArray, number) будет создавать элемент состоящий только из положительных значений, а get_array_of_random_elements(lengthOfArray, number) будет создовать элементы с учётом отрицательных значений.

--- main.py
+++ main.py
@@ -28,3 +28,34 @@ def get_random_letter_and_number_element(number): # элемент из букв
     rand_string = ''.join(random.sample(letters_and_number, number)) # в пустую строку записывает 29ю строку в кол-ве number раз
     return rand_string
     # print("Alphanum Random string of length", number, "is:", rand_string)
+
+array_of_random_elements = []
+
+
+# массив случайных чисел и букв
+def get_array_of_random_elements(lengthOfArray, number):
+    for i in range(lengthOfArray):
+        random_element = str(get_random_number_element(- pow(10, round(number / 2)) + 1, pow(10, round(number / 2)) - 1)) \
+                         + get_random_letter_and_number_element(round(number / 2)) + '.' \
+                         + get_random_letter_and_number_element(number)
+        array_of_random_elements.append(random_element)
+    return array_of_random_elements
+
+
+array_of_random_elements_negative = []
+
+
+# массив случайных чисел и букв с возможно отрицательным значением
+def get_array_of_random_elements_negative(lengthOfArray, number):
+    for i in range(lengthOfArray):
+        random_element = get_random_letter_and_number_element(number) + '.' \
+                         + get_random_letter_and_number_element(number)
+        array_of_random_elements_negative.append(random_element)
+    return array_of_random_elements_negative
+
+
+get_array_of_random_elements(10, 4)
+print(array_of_random_elements)
+
+# get_array_of_random_number_elements(10)
+# print(array_of_random_number_elements)
