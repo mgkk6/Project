1f6a124013cfdcd4dec7f78741ccc99c9bc459c7
mgkk6 <velpeter98@gmail.com>
2021-08-20 17:47:55 +0300

    С помощью модуля string добавил функцию , которая будет хранить в себе строку состоящую из ascii_letters - всех букв и digits - всех цифр

--- main.py
+++ main.py
@@ -20,3 +20,11 @@ def get_array_of_random_number_elements(lengthOfArray):
         random_element = get_random_number_element()
         array_of_random_number_elements.append(random_element)
     return array_of_random_number_elements
+
+
+# элемент из букв и цифр
+def get_random_letter_and_number_element(number): # элемент из букв и цифр
+    letters_and_number = string.ascii_letters + string.digits
+    rand_string = ''.join(random.sample(letters_and_number, number)) # в пустую строку записывает 29ю строку в кол-ве number раз
+    return rand_string
+    # print("Alphanum Random string of length", number, "is:", rand_string)
